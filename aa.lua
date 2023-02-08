1f7ced611932a028c031cc837852e3a617572c11
queered <121299278+Queered@users.noreply.github.com>
2023-02-08 05:42:46 +0300

    Create aa.lua

---
+++ aa.lua
@@ -0,0 +1,32 @@
+getgenv().tog = true
+getgenv().key = "m"
+getgenv().X = 68756
+getgenv().Y = 100
+getgenv().Z = -344
+ 
+game:GetService("RunService").Heartbeat:Connect(function()
+        if getgenv().tog then
+                local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(getgenv().X, getgenv().Y, getgenv().Z)
+                game:GetService("RunService").RenderStepped:Wait()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = vel
+        end
+end)
+ 
+game:GetService("Players").LocalPlayer:GetMouse().KeyDown:Connect(function(keyPressed)
+        if keyPressed == string.lower(getgenv().key) then
+                pcall(function()
+                        if getgenv().tog == false then
+                                getgenv().tog = true
+                                game.StarterGui:SetCore("SendNotification", {
+                                        Title = "aa",
+                                        Text = "AA Enabled" })
+                        elseif getgenv().tog == true then
+                                getgenv().tog = false
+                                game.StarterGui:SetCore("SendNotification", {
+                                        Title = "aa",
+                                        Text = "AA Disabled" })
+                        end
+                end)
+        end
+end)
